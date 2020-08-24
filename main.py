5444b2b650c9b8f02ab2ccef4a3a2f0e9b9d0e2f
Vresod <lukebinkofsky@gmail.com>
2020-08-23 20:43:01 -0400

    added code

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
 #!/usr/bin/env python3
 import discord
+import random
+
+with open("tokenfile", "r") as tokenfile:
+	token=tokenfile.read()
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+	print("logged in as {0.user}".format(client))
+	for guild in client.guilds:
+		if guild.me.nick != "someone":
+			await guild.me.edit(nick="someone")	
+@client.event
+async def on_message(message):
+	if message.author == client.user:
+		return
+	memberlist = message.guild.members
+	if message.guild.me in message.mentions:
+		randomuser = memberlist[random.randint(0,len(memberlist))]
+		await message.channel.send(f"{randomuser.mention}")
+
+client.run(token)
