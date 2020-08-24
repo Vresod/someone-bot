1e132a3d0ffc994eadbccd79acb5ac0018f4e15a
Vresod <lukebinkofsky@gmail.com>
2020-08-23 21:14:24 -0400

    added cliargs also added README

---
+++ README.md
@@ -0,0 +1,3 @@
+# someone bot
+
+this discord bot tags a random person whenever a message tags the bot. unless you remove some of the code it will automatically change its nickname to "someone".
--- main.py
+++ main.py
@@ -1,18 +1,33 @@
 #!/usr/bin/env python3
 import discord
 import random
+from sys import argv as cliargs
 
 with open("tokenfile", "r") as tokenfile:
 	token=tokenfile.read()
 
+changeNick = False if "dontChangeNick" in cliargs else True # default value: True
+includeBots = True if "includeBots" in cliargs else False # default value: False
+print(f"changeNick: {changeNick}\nincludeBots: {includeBots}")
+
 client = discord.Client()
+@client.event
+async def on_guild_join(guild):
+	if(not changeNick):
+		return
+	for guild in client.guilds:
+		if guild.me.nick != "someone":
+			await guild.me.edit(nick="someone")
 
 @client.event
 async def on_ready():
 	print("logged in as {0.user}".format(client))
+	if(not changeNick):
+		return
 	for guild in client.guilds:
 		if guild.me.nick != "someone":
-			await guild.me.edit(nick="someone")	
+			await guild.me.edit(nick="someone")
+
 @client.event
 async def on_message(message):
 	if message.author == client.user:
@@ -20,6 +35,8 @@ async def on_message(message):
 	memberlist = message.guild.members
 	if message.guild.me in message.mentions:
 		randomuser = memberlist[random.randint(0,len(memberlist))]
+		while includeBots or randomuser.bot:
+			randomuser = memberlist[random.randint(0,len(memberlist) - 1)]
 		await message.channel.send(f"{randomuser.mention}")
 
 client.run(token)
